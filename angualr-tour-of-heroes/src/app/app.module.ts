9ae074ce80b5cac49c6e57b14cf2a291a384631d
caohuijubn2018 <caohuijun2018@gmail.com>
2021-07-13 18:39:32 +0800

    学习到第四部分，仍然需要复看

--- angualr-tour-of-heroes/src/app/app.module.ts
+++ angualr-tour-of-heroes/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+//实现数据和属性双向绑定之前需要导入 FormsModule
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angualr-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angualr-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../Hero'
+// import  {HEROES} from '../mock-heroes'
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,13 +11,29 @@ import {Hero} from '../Hero'
 export class HeroesComponent implements OnInit {
  // hero = 'Windstorm'
 
+
+ heroes : Hero[] = [];
  hero  : Hero  = {
    id : 1,
    name : 'Windstrom'
  }
-  constructor() { }
 
+
+selectedHero ?: Hero;
+onSelect(hero : Hero) : void {
+  this.selectedHero = hero
+}
+
+  // constructor() { } 
+  constructor(private heroService : HeroService, private messageService : MessageService) {} //注入点
+
+  getHeroes() :void {
+    // this.heroes = this.heroService.getHeroes(); //从服务器中获取数据
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)//改用异步的方法
+  }
   ngOnInit(): void {
+    this.getHeroes()
   }
 
+  
 }

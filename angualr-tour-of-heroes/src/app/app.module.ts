9fbd392781dd319c7d97fe887d5d3c951226165a
caohuijubn2018 <caohuijun2018@gmail.com>
2021-07-14 15:18:04 +0800

    从http获取数据

--- angualr-tour-of-heroes/src/app/app.module.ts
+++ angualr-tour-of-heroes/src/app/app.module.ts
@@ -10,18 +10,31 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+//HttpClient时Angular通过HTTP与远程服务器通讯的机制
+import { HttpClientModule} from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessageComponent,
-    DashboardComponent
+    DashboardComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angualr-tour-of-heroes/src/app/hero.service.ts
+++ angualr-tour-of-heroes/src/app/hero.service.ts
@@ -4,23 +4,52 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService : MessageService) { }  //服务中的服务
+  constructor(private messageService : MessageService , private http : HttpClient) { }  //服务中的服务
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes  = of(HEROES) //of(HEROES)会返回一个Observable<Hero[]>
-    this.messageService.add('HeroService : fetched heroes')
-    return heroes;
-  }
-
-  getHero(id : number) : Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`Heroseverce:fetched hero id=${id}`)
-    return of(hero)
-  }
+  // getHeroes(): Observable<Hero[]> {
+  //   const heroes  = of(HEROES) //of(HEROES)会返回一个Observable<Hero[]>
+  //   this.messageService.add('HeroService : fetched heroes')
+  //   return heroes;
+  // }
   
+  getHeroes():Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes',[]))
+    )
+  }
+ 
+  // getHero(id : number) : Observable<Hero> {
+  //   const hero = HEROES.find(h => h.id === id)!;
+  //   this.messageService.add(`Heroseverce:fetched hero id=${id}`)
+  //   return of(hero)
+  // }
+  getHero(id : number) : Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched heroe id = ${id}`)),
+      catchError(this.handleError<Hero>(`getHero id = ${id}`))
+    )
+  }
+   
+  private heroesUrl = 'api/heroes';  // URL to web api
+  private log(message :string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+  private handleError<T>(operators = 'operation',result ?: T) {
+    return (error:any) : Observable<T> => {
+      console.log(error);
+      this.log(`${operators} failed : ${error.message}`);
+      return of(result as T)
+    }
+  }
 }
